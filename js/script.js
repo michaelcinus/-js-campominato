e8db4a25e04b214aa7461883b7d598bf028800e8
Michael Cinus <michaelcinus@gmail.com>
2020-06-30 16:18:34 +0200

    correzioni, conteggio punti e comunica verdetto

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //GENERA 16 numeri casuali tra 1 e 100 (no duplicati)
-var numeriRandom = [generaNumeroRandom(1, 100)];
+var numeriRandom = [];
 var random;
 for (var i = 0; i < 4; ++i) {
     random = generaNumeroRandom(1, 100);
@@ -9,22 +9,12 @@ for (var i = 0; i < 4; ++i) {
     } else {
         i--;
     }
-    /*var nuovoNumero = true;
-    for (var cont = 0; cont < i; cont++){
-        if (numeriRandom[cont] == random){
-            nuovoNumero = false;
-        } else if (nuovoNumero == true) {
-            numeriRandom[i] = random;
-        } else {
-            i--;
-        }
-    } */
 }
 console.log(numeriRandom);
 
 
 //CHIEDI all'utente di inserire (100 - 16)numeri tra 1 e 100 (no duplicati)
-var numeriInseriti = [parseInt(prompt("inserisci un numero da 1 a 100"))];
+var numeriInseriti = [];
 var punteggio = 0;
 
 for (var i = 0; i < 4; ++i) {
@@ -45,7 +35,7 @@ for (var i = 0; i < 4; ++i) {
     }
     
     if (ris == true) {
-        document.getElementById("h1").innerHTML = "HAI PERSO! hai preso una bomba";
+        document.getElementById("h1").innerHTML = "HAI PERSO! una bomba è esplosa. Hai totalizzato " + punteggio + " punti";
         break;
     } else{
         punteggio++;
@@ -56,6 +46,10 @@ console.log(punteggio);
 
 
 //COMUNICA all'utente il punteggio
+if (punteggio == numeriInseriti.length){
+    document.getElementById("risultato").innerHTML = "HAI VINTO! hai totalizzato " + punteggio + " punti";
+
+}
 
 //FUNCTION genera numeri casuali
 function generaNumeroRandom(min, max) {
