28aa286e78b63159a5916e556ca8adf370dc9815
Michael Cinus <michaelcinus@gmail.com>
2020-06-30 15:52:11 +0200

    JS: correzioni GENERA & CHIEDI all'utente numeri

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@
 var numeriRandom = [generaNumeroRandom(1, 100)];
 var random;
 for (var i = 0; i < 4; ++i) {
-    random = generaNumeroRandom(1, 100)
-    var nuovoNumero = true;
+    random = generaNumeroRandom(1, 100);
+    var boolean = numeriRandom.includes(random);
+    if (boolean == false) {
+        numeriRandom[i] = random;
+    } else {
+        i--;
+    }
+    /*var nuovoNumero = true;
     for (var cont = 0; cont < i; cont++){
         if (numeriRandom[cont] == random){
             nuovoNumero = false;
@@ -12,12 +18,25 @@ for (var i = 0; i < 4; ++i) {
         } else {
             i--;
         }
-    }
+    } */
 }
 console.log(numeriRandom);
 
 
 //CHIEDI all'utente di inserire (100 - 16)numeri tra 1 e 100 (no duplicati)
+var numeriInseriti = [parseInt(prompt("inserisci un numero da 1 a 100"))];
+
+for (var i = 0; i < 4; ++i) {
+    inserito = parseInt(prompt("inserisci un numero da 1 a 100"));
+    var boolean = numeriInseriti.includes(inserito)
+    if (boolean == false) {
+        numeriInseriti[i] = inserito;
+    } else {
+        i--;
+    }
+}  
+console.log(numeriInseriti);
+
 
 
 
@@ -29,6 +48,6 @@ function generaNumeroRandom(min, max) {
     max = Math.floor(max);
     var result = Math.floor(Math.random() * (max - min + 1)) + min;
     return result;
-  }
+}
 
 //FUNCTION chiedi e verifica numeri
