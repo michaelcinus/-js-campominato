1b9ee48ecaf95655a9b361392b6100deacd7b313
Michael Cinus <michaelcinus@gmail.com>
2020-07-01 12:59:46 +0200

    JS:conversion for in while elimina "break" e "i--"

--- js/script.js
+++ js/script.js
@@ -18,47 +18,48 @@ switch (difficoltà) {
     default:
      alert ("La difficoltà inserita non esiste")
 }
-console.log(max);
 
 //GENERA 16 numeri casuali tra 1 e max (no duplicati)
 var numeriRandom = [];
 var random;
-for (var i = 0; i < 16; ++i) {
+var i = 0;
+while (i < 16) {
     random = generaNumeroRandom(min, max);
     if (inArray(numeriRandom, random) !=true) {
-        numeriRandom[i] = random;
-    } else {
-        i--;
+        numeriRandom.push(random);
     }
+    i++;
 }
 console.log(numeriRandom);
 
 //CHIEDI all'utente di inserire (max - 16)numeri tra 1 e max (no duplicati)
 var numeriInseriti = [];
 var punteggio = 0;
+var i = 0; 
+var bomba = false;
 
-for (var i = 0; i < max - 16; ++i) {
+while (i < max - 16 && bomba == false) {
     inserito = parseInt(prompt("inserisci un numero da 1 a " + max));
     if (inArray(numeriInseriti, inserito) !=true) {
-        numeriInseriti[i] = inserito;
+        numeriInseriti.push(inserito);
     } else {
         alert("Numero già inserito");
-        i--;
     }
 
     //VERIFICO se il numero inserito non sia superiore del range max
     if (inserito > max || inserito < min || isNaN(inserito)) {
         alert("Numero inserito non è valido")
-        i--
+        numeriInseriti.pop(inserito);
     } else {
         //VERIFICO se il numero inserito dall'utente è presente nell'array generato
         if (inArray(numeriRandom, inserito)) {
             document.getElementById("h1").innerHTML = "HAI PERSO! una bomba è esplosa. Hai totalizzato " + punteggio + " punti";
-            break;
+            bomba = true;
         } else{
             punteggio++;
         }
     }
+    i++;
 }  
 console.log(numeriInseriti);
 console.log(punteggio);
@@ -66,7 +67,6 @@ console.log(punteggio);
 //COMUNICA all'utente il punteggio
 if (punteggio == numeriInseriti.length){
     document.getElementById("h1").innerHTML = "HAI VINTO! hai totalizzato " + punteggio + " punti";
-
 }
 
 //FUNCTION genera numeri casuali
